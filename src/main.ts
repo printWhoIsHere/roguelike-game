b6d0f48ee84320e3ce4c2a7f21f7b3a41e8308e5
printWhoIsHere <uvarov_2011@list.ru>
2025-03-01 03:44:50 +0300

    Загрузка и обработка ошибки

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,31 @@ import '@styles/main.scss'
 import { Game } from '@/core/game'
 import { StatusBar } from '@ui/status-bar'
 
-document.addEventListener('DOMContentLoaded', () => {
-	const appContainer = document.createElement('div')
-	appContainer.id = 'app'
+document.addEventListener('DOMContentLoaded', async () => {
+	const loadingElement = document.createElement('div')
+	loadingElement.id = 'loading'
+	loadingElement.innerText = 'Loading...'
+	document.body.appendChild(loadingElement)
 
-	const statusBar = new StatusBar()
-	const canvas = document.createElement('canvas')
+	try {
+		const appContainer = document.createElement('div')
+		appContainer.id = 'app'
 
-	appContainer.appendChild(statusBar.getElement())
-	appContainer.appendChild(canvas)
+		const statusBar = new StatusBar()
+		const canvas = document.createElement('canvas')
 
-	document.body.appendChild(appContainer)
+		appContainer.appendChild(statusBar.getElement())
+		appContainer.appendChild(canvas)
 
-	const game = new Game(canvas)
-	game.start()
+		const game = new Game(canvas)
+		await game.init()
+		game.start()
+
+		document.body.appendChild(appContainer)
+	} catch (error) {
+		console.error('Ошибка инициализации игры', error)
+		loadingElement.innerText = 'Error loading game'
+	} finally {
+		document.body.removeChild(loadingElement)
+	}
 })
